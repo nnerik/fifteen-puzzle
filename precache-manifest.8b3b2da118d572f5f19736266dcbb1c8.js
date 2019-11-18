5912b1164e040ac5de08f6c37ad03598036212ef
Erik Haugan <erik@haugan.no>
2019-11-18 20:59:24 +0100

    Updates

---
+++ precache-manifest.8b3b2da118d572f5f19736266dcbb1c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2813849bdeba9795408193bcc247aa8",
+    "url": "/fifteen-puzzle/index.html"
+  },
+  {
+    "revision": "2af003298d2491fadb59",
+    "url": "/fifteen-puzzle/static/css/main.7bdd3565.chunk.css"
+  },
+  {
+    "revision": "ed9528fdd749f28d8895",
+    "url": "/fifteen-puzzle/static/js/2.d1d946ed.chunk.js"
+  },
+  {
+    "revision": "2af003298d2491fadb59",
+    "url": "/fifteen-puzzle/static/js/main.3e9b1544.chunk.js"
+  },
+  {
+    "revision": "b58ea441a187dceb460a",
+    "url": "/fifteen-puzzle/static/js/runtime-main.0d38f1b9.js"
+  }
+]);
